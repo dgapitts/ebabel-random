03b8cd0dc52c8e7de2317cfb0e97f042fa4f4d7d
Dave Pitts <dgapitts@gmail.com>
2018-09-29 16:47:21 +0100

    mark min parameter as optional ie nullable

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  * `random`
  * Returns a random integer number from 1 to 100 or from min to max (min is non-inclusive).
  * @param {number} max - the maximum to be returned, should be an integer
- * @param {number} min - the minimum to be returned, should be an integer
- * @returns {number} Integer between min and max (inclusively)
+ * @param {number?} min - the minimum to be returned, should be an integer
+ * @returns {number} Integer between min and max
  */
 const random = (max = 100, min = 0) => {
   if (min == undefined) {min=0}
